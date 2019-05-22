70a53cc8e6db910c56a10c17be486bd65da7444b
cadaverine <kennville@gmail.com>
2019-05-22 23:40:55 +0300

    feat(RequestsQueue.vue): добавлен компонент очереди заявок

---
+++ components/RequestsQueue.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="queue">
+    <Request
+      v-for="(_, index) of Array(length).fill(0)"
+      :key="index"
+    />
+  </div>
+</template>
+
+<script>
+import Request from './Request.vue'
+
+export default {
+  components: {
+    Request
+  },
+  props: {
+    length: {
+      type: Number,
+      default: 10
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.queue {
+  height: 70px;
+  width: 300px;
+  display: flex;
+  border: 2px solid #479CFF;
+}
+</style>
