ae5576f7a5fb4b5d687051ffdd137d7971fc8f1f
cadaverine <kennville@gmail.com>
2019-05-23 23:21:48 +0300

    feat(RequestsQueue.vue): обновлена реализация

--- components/RequestsQueue.vue
+++ components/RequestsQueue.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="queue">
-    <Request
-      v-for="(_, index) of Array(length).fill(0)"
-      :key="index"
-    />
+    <div class="queue__container">
+      <Request
+        v-for="(_, index) of Array(length).fill(0)"
+        :key="index"
+        :type="getRandomRequestType()"
+        :coords="{ top: 3, left: 460 - index * (45 / 2) }"
+      />
+    </div>
   </div>
 </template>
 
@@ -17,7 +21,20 @@ export default {
   props: {
     length: {
       type: Number,
-      default: 10
+      default: 21
+    }
+  },
+  data() {
+    return {
+      requestTypes: ['XCHG', 'CARD', 'CRED', 'ACNT']
+    }
+  },
+  methods: {
+    getRandomRequestType() {
+      const { length } = this.requestTypes
+      const type = this.requestTypes[(Math.round(Math.random() * 10) % length)]
+
+      return type
     }
   }
 }
@@ -25,9 +42,21 @@ export default {
 
 <style lang="scss">
 .queue {
-  height: 70px;
-  width: 300px;
-  display: flex;
+  top: 230px;
+  left: 550px;
+  width: 500px;
+  height: 33px;
   border: 2px solid #479CFF;
+  display: flex;
+  position: absolute;
+  overflow-x: visible;
+}
+
+.queue__container {
+  left: -1%;
+  width: 102%;
+  height: 100%;
+  position: absolute;
+  background-color: white;
 }
 </style>
